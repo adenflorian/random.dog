4d09a7328a71a4a889a8e4a9990dda2378af1a2a
David Valachovic <AdenFlorian@gmail.com>
2017-04-07 23:33:17 -0500

    some tweaks

--- randomdog.js
+++ randomdog.js
@@ -1,101 +1,75 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+const express = require("express")
+const hbs = require("handlebars")
+const fs = require("fs")
 
-const hostname = 'random.dog';
-const port = 8080;
+// Should be run behind a reverse proxy
+const domain = "random.dog"
+const privatePort = 8080
+const host = `https://random.dog`
 
-const server = http.createServer((req, res) => {
-	var request = url.parse(req.url, true);
-	var action = request.pathname;
+Array.prototype.random = function() {
+    return this[Math.floor(Math.random() * this.length)]
+}
+
+var app = express()
+
+app.use(function(req, res, next) {
 	console.log('NEW REQUEST: ' + getDateTime() + ' EST - NYC')
-	console.log('requestor IP: ' + req.connection.remoteAddress);
-	console.log('action: ' + action);
+	console.log('requestor IP: ' + req.connection.remoteAddress)
+	console.log('action: ' + req.url)
+	next()
+})
 
-	if (action == '/robots.txt') {
-		console.log('robots');
-		try {
-			var robots = fs.readFileSync('robots.txt');
-			res.writeHead(200, {'Content-Type': 'text/plain' });
-			res.end(robots);
-		} catch(err) {
-			res.writeHead(200, {'Content-Type': 'text/plain' });
-			res.end('something borked');
-		}
-	} else if (action == '/woof') {
-		try {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'text/plain');
+var helloworld = hbs.compile(fs.readFileSync("./helloworld.hbs", "utf8"))
 
-			var fileNames = fs.readdirSync('img');
+var cache = fs.readdirSync("./img")
 
-			var randNum = Math.floor((Math.random() * fileNames.length));
-			var fileName = fileNames[randNum];
-			console.log('randNum: ' + randNum);
-			console.log('fileName: ' + fileName);
+var refresher = setInterval(() => {
+	fs.readdir("./img", (err, files) => {
+		if (err) return console.error(err.stack)
+		cache = files
+	})
+}, 20000)
 
-			res.end(fileName);
-		} catch(err) {
-			res.writeHead(200, {'Content-Type': 'text/plain' });
-			res.end('something borked');
-		}
-	} else if (action != '/') {
-		try {
-			var img = fs.readFileSync('img' + action);
-			if (action.endsWith(".mp4")) {
-				res.writeHead(200, { 'Content-Type': 'video/mp4' });
-			} else {
-				res.writeHead(200, {'Content-Type': 'image/jpg' });
-			}
-			res.end(img, 'binary');
-		} catch(err) {
-			res.writeHead(200, {'Content-Type': 'text/plain' });
-			res.end('something borked');
-		}
-	} else { 
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/html');
+// API
+app.get("/woof.json", (req, res) => {
+	res.status(200).json({
+		url: `${host}/${cache.random()}`
+	})
+})
+app.get("/woof", (req, res) => {
+	res.status(200).send(cache.random())
+})
 
-		var fileNames = fs.readdirSync('img');
+app.get("/", (req, res) => {
+	res.status(200).send(helloworld({dog: `/${cache.random()}`}))
+})
+app.get("*", express.static("./img"))
 
-		var randNum = Math.floor((Math.random() * fileNames.length));
-		var fileName = fileNames[randNum];
-		console.log('randNum: ' + randNum);
-		console.log('fileName: ' + fileName);
-
-		if (fileName.endsWith(".mp4")) {
-			res.end(`<html><p>Hello World, This Is Dog</p><p><video preload="auto" autoplay="autoplay" muted="muted" loop="loop" webkit-playsinline=""><source src='${fileName}' type='video/mp4'>Your browser does not support the video tag.</video></p><footer>@AdenFlorian</footer>`);
-		} else {
-			res.end(`<html><p>Hello World, This Is Dog</p><p><img src='${fileName}'></img></p><footer>@AdenFlorian</footer>`);
-		}
-	}
-});
-
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(privatePort, (err) => {
+	if (err) return console.error(err.stack)
+	console.log(`Dogs barking at ${host}`)
+})
 
 function getDateTime() {
+    var date = new Date()
 
-    var date = new Date();
+    var hour = date.getHours()
+    hour = (hour < 10 ? "0" : "") + hour
 
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
+    var min  = date.getMinutes()
+    min = (min < 10 ? "0" : "") + min
 
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
+    var sec  = date.getSeconds()
+    sec = (sec < 10 ? "0" : "") + sec
 
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
+    var year = date.getFullYear()
 
-    var year = date.getFullYear();
+    var month = date.getMonth() + 1
+    month = (month < 10 ? "0" : "") + month
 
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
+    var day  = date.getDate()
+    day = (day < 10 ? "0" : "") + day
 
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-
-    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
-
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec
 }
