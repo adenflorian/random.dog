5a181431dc177e5dde05d0ef5169a2538e02a217
David Valachovic <AdenFlorian@gmail.com>
2017-10-28 14:42:41 -0500

    error logging and formatting

--- randomdog.js
+++ randomdog.js
@@ -112,6 +112,7 @@ app.get('/doggos', (req, res) => {
 	req.visitor.pageview(req.path).send()
 	fs.readdir('./' + approvedDogFolderName, (err, files) => {
 		if (err) {
+			console.log(err)
 			res.status(500).send()
 		} else {
 			res.status(200).json(files)
@@ -129,8 +130,8 @@ app.get('*', (req, res, next) => {
 })
 
 app.get('/favicon.*', (req, res, next) => {
-	req.visitor.pageview("/favicon.*").send()
-	express.static(".")(req, res, next)
+	req.visitor.pageview('/favicon.*').send()
+	express.static('.')(req, res, next)
 })
 
 app.get('/upload', (req, res) => {
@@ -238,21 +239,21 @@ function getDateTime() {
 	var date = new Date()
 
 	var hour = date.getHours()
-	hour = (hour < 10 ? "0" : "") + hour
+	hour = (hour < 10 ? '0' : '') + hour
 
 	var min = date.getMinutes()
-	min = (min < 10 ? "0" : "") + min
+	min = (min < 10 ? '0' : '') + min
 
 	var sec = date.getSeconds()
-	sec = (sec < 10 ? "0" : "") + sec
+	sec = (sec < 10 ? '0' : '') + sec
 
 	var year = date.getFullYear()
 
 	var month = date.getMonth() + 1
-	month = (month < 10 ? "0" : "") + month
+	month = (month < 10 ? '0' : '') + month
 
 	var day = date.getDate()
-	day = (day < 10 ? "0" : "") + day
+	day = (day < 10 ? '0' : '') + day
 
-	return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec
+	return year + ':' + month + ':' + day + ':' + hour + ':' + min + ':' + sec
 }
