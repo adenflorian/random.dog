02d7149bfba505037f4aed2fe2b08d322b1f58e7
David Valachovic <David.E.Valachovic@gmail.com>
2017-11-18 16:33:17 -0600

    unit test for /woof

--- test/randomdog-tests.js
+++ test/randomdog-tests.js
@@ -17,7 +17,18 @@ describe('randomdog', () => {
                 .expect('Content-Type', /json/)
                 .expect(200)
                 .then(response => {
-                    expect(response.body.url).to.startWith('testhost/testdog.jpg')
+                    expect(response.body.url).to.equal('testhost/testdog.jpg')
+                })
+        })
+    })
+    describe('get /woof', () => {
+        it('should go woof', () => {
+            return request(createApp('testhost'))
+                .get('/woof')
+                .expect('Content-Type', /text\/html/)
+                .expect(200)
+                .then(response => {
+                    expect(response.text).to.equal('testdog.jpg')
                 })
         })
     })
