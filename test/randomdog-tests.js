fb6bd4edae619ee93b10731380c482f5d0b4a234
David Valachovic <David.E.Valachovic@gmail.com>
2019-07-12 16:26:04 -0500

    cleaning tests

--- test/randomdog-tests.js
+++ test/randomdog-tests.js
@@ -19,18 +19,20 @@ describe('randomdog', () => {
     })
     describe('get /woof', () => {
         it('should go woof', async () => {
-            fsLayer.getGoodDogs.resolves(['testdog.jpg'])
-            return request(await createApp('testhost'))
+            // @ts-ignore
+            fsLayer.getGoodDogs.resolves(['testDog.jpg'])
+            return request(await createApp('test_host'))
                 .get('/woof')
                 .expect('Content-Type', /text\/html/)
                 .expect(200)
                 .then(response => {
-                    expect(response.text).to.equal('testdog.jpg')
+                    expect(response.text).to.equal('testDog.jpg')
                 })
         })
         it('should not return jpeg dogs when filter is jpg', async () => {
-            fsLayer.getGoodDogs.resolves(['testdog.JPG', 'dog.png', 'testdog.jpg'])
-            return request(await createApp('testhost'))
+            // @ts-ignore
+            fsLayer.getGoodDogs.resolves(['testDog.JPG', 'dog.png', 'testDog.jpg'])
+            return request(await createApp('test_host'))
                 .get('/woof?filter=jpg')
                 .expect('Content-Type', /text\/html/)
                 .expect(200)
@@ -41,58 +43,62 @@ describe('randomdog', () => {
     })
     describe('get /woof.json', () => {
         it('should go woof', async () => {
-            fsLayer.getGoodDogs.resolves(['testdog.jpg'])
-            return request(await createApp('testhost'))
+            // @ts-ignore
+            fsLayer.getGoodDogs.resolves(['testDog.jpg'])
+            return request(await createApp('test_host'))
                 .get('/woof.json')
                 .expect('Content-Type', /json/)
                 .expect(200)
                 .then(response => {
-                    expect(response.body.url).to.equal('testhost/testdog.jpg')
+                    expect(response.body.url).to.equal('test_host/testDog.jpg')
                 })
         })
         it('should not return jpeg dogs when filter is jpg', async () => {
-            fsLayer.getGoodDogs.resolves(['testdog.jpg', 'dog.png', 'testdog.JPG'])
-            return request(await createApp('testhost'))
+            // @ts-ignore
+            fsLayer.getGoodDogs.resolves(['testDog.jpg', 'dog.png', 'testDog.JPG'])
+            return request(await createApp('test_host'))
                 .get('/woof.json?filter=jpg')
                 .expect('Content-Type', /json/)
                 .expect(200)
                 .then(response => {
-                    expect(response.body.url).to.equal('testhost/dog.png')
+                    expect(response.body.url).to.equal('test_host/dog.png')
                 })
         })
     })
     describe('get /doggos', () => {
         it('should return a lot of dogs', async () => {
-            fsLayer.getGoodDogs.resolves(['doga', 'dogb'])
-            return request(await createApp('testhost'))
+            // @ts-ignore
+            fsLayer.getGoodDogs.resolves(['dogA', 'dogB'])
+            return request(await createApp('test_host'))
                 .get('/doggos')
                 .expect('Content-Type', /application\/json/)
                 .expect(200)
                 .then(response => {
-                    expect(response.body).to.deep.equal(['doga', 'dogb'])
+                    expect(response.body).to.deep.equal(['dogA', 'dogB'])
                 })
         })
         it('should return filtered dogs', async () => {
-            fsLayer.getGoodDogs.resolves(['doga.Jpg', 'dogb.png'])
-            return request(await createApp('testhost'))
+            // @ts-ignore
+            fsLayer.getGoodDogs.resolves(['dogA.Jpg', 'dogB.png'])
+            return request(await createApp('test_host'))
                 .get('/doggos?filter=jpg')
                 .expect('Content-Type', /application\/json/)
                 .expect(200)
                 .then(response => {
-                    expect(response.body).to.deep.equal(['dogb.png'])
+                    expect(response.body).to.deep.equal(['dogB.png'])
                 })
         })
     })
     describe('get *', () => {
         it('should return 401 when referred from /review and no bone', async () => {
-            return request(await createApp('testhost'))
+            return request(await createApp('test_host'))
                 .get('/abc.jpg')
                 .set('Referer', 'http://example.com/review')
                 .expect(401)
         })
         it('should return 200 when referred from /review and good bone', async () => {
             sandbox.stub(express, 'static').callsFake(() => (req, res) => res.send(200))
-            return request(await createApp('testhost'))
+            return request(await createApp('test_host'))
                 .get('/myNewDog.mp4')
                 .set('Cookie', 'bone=' + Buffer.from('goodKey').toString('base64'))
                 .set('Referer', 'http://example.com/review')
@@ -100,7 +106,7 @@ describe('randomdog', () => {
         })
         it('should return 200 when not referred from /review', async () => {
             sandbox.stub(express, 'static').callsFake(() => (req, res) => res.send(200))
-            return request(await createApp('testhost'))
+            return request(await createApp('test_host'))
                 .get('/myNewDog.mp4')
                 .expect(200)
         })
@@ -110,17 +116,17 @@ describe('randomdog', () => {
             ['get', 'post'].forEach(method => {
                 describe(method, () => {
                     it('should return 401 when bone cookie not present', async () => {
-                        return request(await createApp('testhost'))[method]('/review')
+                        return request(await createApp('test_host'))[method]('/review')
                             .set('Cookie', 'treat=goodKey')
                             .expect(401)
                     })
                     it('should return 401 when bone cookie is badKey', async () => {
-                        return request(await createApp('testhost'))[method]('/review')
+                        return request(await createApp('test_host'))[method]('/review')
                             .set('Cookie', 'bone=badKey')
                             .expect(401)
                     })
                     it('should return 401 when bone cookie is decoded goodKey', async () => {
-                        return request(await createApp('testhost'))[method]('/review')
+                        return request(await createApp('test_host'))[method]('/review')
                             .set('Cookie', 'bone=' + 'goodKey')
                             .expect(401)
                     })
@@ -130,7 +136,7 @@ describe('randomdog', () => {
         describe('authorized', () => {
             describe('get', () => {
                 it('should return 200 when bone cookie is encoded goodKey', async () => {
-                    return request(await createApp('testhost'))
+                    return request(await createApp('test_host'))
                         .get('/review')
                         .set('Cookie', 'bone=' + Buffer.from('goodKey').toString('base64'))
                         .expect(200)
@@ -138,7 +144,7 @@ describe('randomdog', () => {
             })
             describe('post', () => {
                 it('should return 200 when bone cookie is encoded goodKey', async () => {
-                    return request(await createApp('testhost'))
+                    return request(await createApp('test_host'))
                         .post('/review')
                         .set('Cookie', 'bone=' + Buffer.from('goodKey').toString('base64'))
                         .send({action: 'adopt', dogName: 'cliff'})
